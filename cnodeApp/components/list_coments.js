d74c06f88236e73f053ec9e8289c1c2a70addb6b
AChuang <lichuang516@126.com>
2018-01-25 18:10:48 +0800

    完善

--- cnodeApp/components/list_coments.js
+++ cnodeApp/components/list_coments.js
@@ -5,11 +5,11 @@
 	var comm_temlate_list = 
 			   '<ul id="dataList" class="mui-table-view">'+
 					'<li class="mui-table-view-cell mui-media" v-for="item in data">'+
-						'<div class="mui-media-object mui-pull-left"><img class="mui-media-object" v-lazy="item.cover">'+
+						'<div @tap="to_user(item.loginname)" class="mui-media-object mui-pull-left"><img class="mui-media-object" v-lazy="item.cover">'+
 						'<span class="floor font_12">{{item.index}} 楼</span></div>'+
 						'<div class="mui-media-body">'+
-							'<p class="font_12">{{item.loginname}}<span class="mui-pull-right">{{item.time}}</span></p>'+
-							'<p class="content" v-html= "item.content" >s</p>'+
+							'<p class="font_12 color_333">{{item.loginname}}<span class="mui-pull-right">{{item.time}}</span></p>'+
+							'<p class="content"><span v-html="item.content"></span></p>'+
 						'</div>'+
 					'</li>'+
 				'</ul>';
@@ -20,6 +20,10 @@
 		data: function() {
 			return {
 				data: []
+			}
+		},methods:{
+			to_user:function(loginname){
+				console.log('loginname=====' + loginname);
 			}
 		}
 	})
