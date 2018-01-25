10b6689e201ac96f779db3859ae18101fb4103a5
AChuang <lichuang516@126.com>
2018-01-25 16:56:27 +0800

    评论列表

--- cnodeApp/components/list_coments.js
+++ cnodeApp/components/list_coments.js
@@ -2,27 +2,25 @@
  * @description 评论数据列表组件
  * */
 (function(V, name,_) {
-	var temlate_list = 
+	var comm_temlate_list = 
 			   '<ul id="dataList" class="mui-table-view">'+
-					'<li class="mui-table-view-cell mui-media v-for="item in data">'+
+					'<li class="mui-table-view-cell mui-media" v-for="item in data">'+
 						'<div class="mui-media-object mui-pull-left"><img class="mui-media-object" v-lazy="item.cover">'+
 						'<span class="floor font_12">{{item.index}} 楼</span></div>'+
 						'<div class="mui-media-body">'+
 							'<p class="font_12">{{item.loginname}}<span class="mui-pull-right">{{item.time}}</span></p>'+
-							'<p class="content">{{item.content}}</p>'+
+							'<p class="content" v-html= "item.content" >s</p>'+
 						'</div>'+
 					'</li>'+
 				'</ul>';
 
 	V.component(name, {
-		template: temlate_list,
+		template: comm_temlate_list,
 		props: ['data'],
 		data: function() {
 			return {
 				data: []
 			}
-		},
-		methods: {
 		}
 	})
-}(Vue, 'app-comlist',mui))
+}(Vue, 'app-list',mui))
